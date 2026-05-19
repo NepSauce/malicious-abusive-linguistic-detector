74f355468a6a22ac423d03234fc1671e3e576ea4
NepSauce <zawadatif.builds@gmail.com>
2026-05-19 16:37:49 -0300

    Fix: Fix instance of char tokenization instead of word

--- mald.py
+++ mald.py
@@ -3,7 +3,7 @@ from preprocessing.vocabulary import Vocabulary
 
 text = "Hello, World! This is a   !!!!test."
 tokens = Tokenizer().tokenize(text)
-index, words = Vocabulary().build_vocab(tokens)
+index, words = Vocabulary().build_vocab([tokens])
 
 print(index)
 print(words)
