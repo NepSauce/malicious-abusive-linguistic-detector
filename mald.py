97c02c53cce6a7c9d128330b6de0274328189e15
NepSauce <zawadatif.builds@gmail.com>
2026-05-19 13:43:24 -0300

    Feat: Add build_vocab method and add verification

--- mald.py
+++ mald.py
@@ -1,6 +1,11 @@
 from preprocessing.tokenizer import Tokenizer
+from utils.vocabulary import Vocabulary
 
 text = "Hello, World! This is a   !!!!test."
 tokens = Tokenizer().tokenize(text)
+index, words = Vocabulary().build_vocab(tokens)
 
-print(tokens)
+print(index)
+print(words)
+
+
--- utils/vocabulary.py
+++ utils/vocabulary.py
@@ -7,3 +7,15 @@ class Vocabulary:
         self.word_counts = Counter()
         self.vocab_size = 0
 
+    def build_vocab(self, tokenized_sentences):
+        for sentences in tokenized_sentences:
+            self.word_counts.update(sentences)
+
+        for word in self.word_counts:
+            idx = self.vocab_size
+            self.word_index[word] = idx
+            self.index_word[idx] = word
+            self.vocab_size += 1
+        
+        return self.word_index, self.index_word
+
