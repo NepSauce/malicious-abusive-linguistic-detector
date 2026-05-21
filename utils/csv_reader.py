a57a3937cc567fffa344c009ecf0ac20726c5c64
NepSauce <zawadatif.builds@gmail.com>
2026-05-21 16:39:30 -0300

    Feat: Add CSV reader

---
+++ utils/csv_reader.py
@@ -0,0 +1,18 @@
+import csv
+
+class CSVReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_csv(self):
+        try:
+            with open(self.file_path, mode='r', newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                data = [row for row in reader]
+
+        except csv.Error as e:
+            print(f"Error reading CSV file: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+        return data
