c6cc58c29ddfb5cd4e8224f9842d2d923b32a4f7
NepSauce <zawadatif.builds@gmail.com>
2026-05-21 16:18:27 -0300

    Feat: Add initial structure for Preprocessor class

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,6 +1,9 @@
-
+from preprocessing.tokenizer import Tokenizer
+from preprocessing.vocabulary import Vocabulary
 
 class Preprecessor:
     def __init__(self):
         pass
     
+    def preprocess(self, dataset):
+        pass
