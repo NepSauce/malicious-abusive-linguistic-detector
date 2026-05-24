bef3a73fc61d62237136a9a56f6fcf91843d471b
NepSauce <zawadatif.builds@gmail.com>
2026-05-24 01:16:37 -0300

    Chore: Move padding sequences into vocabulary.py

--- preprocessing/vocabulary.py
+++ preprocessing/vocabulary.py
@@ -45,3 +45,11 @@ class Vocabulary:
             words.append(self.index_word[index])
         
         return words
+    
+    def pad_sequence(sequence, max_length, pad_value=0):
+        sequence = sequence[:max_length]
+        padding_length = max_length - len(sequence)
+        padding = [pad_value] * padding_length
+        padded_sequence = sequence + padding
+
+        return padded_sequence
