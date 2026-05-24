81c768014a003f10d93ebbbf36856c8f9ce056b6
NepSauce <zawadatif.builds@gmail.com>
2026-05-24 02:24:37 -0300

    Fix: Fix preprocessing pipeline to preserve labels

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -10,11 +10,21 @@ class Preprocessor:
         self.file_path = file_path
 
     def preprocess(self):
+        processed_data = []
         data = self.csv_reader.read_csv()
         clean_text = self.tokenizer.clean_text(data)
+
+        for sequence in clean_text:
+            label = sequence[1]
+            tokens = self.tokenizer.tokenize(sequence[0])
+            self.vocabulary.build_vocab([tokens])
+            encoded_tokens = self.vocabulary.encode(tokens)
+
+            processed_data.append((encoded_tokens, label))
+
         # tokens = self.tokenizer.tokenize(clean_text)
         # self.vocabulary.build_vocab(tokens)
         # encoded_tokens = [self.vocabulary.encode(token) for token in tokens]
 
-        return clean_text
+        return processed_data
     
